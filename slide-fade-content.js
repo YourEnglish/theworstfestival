2dec045db59f0d479903fe6112c10fb6d54dedc4
Adriaan Bol <adriaan.bol@runbox.com>
2015-04-21 16:16:55 +0200

    2nd attempt om nieuw file online te krijgen

--- slide-fade-content.js
+++ slide-fade-content.js
@@ -1,6 +1,7 @@
 // Ajax Slide & Fade Content with jQuery v2.0 @ http://perishablepress.com/slide-fade-content/
 // DEMO @ http://perishablepress.com/wp/wp-content/demos/slide-fade-content/
 // DEMO @ http://perishablepress.com/demos/slide-fade-content/
+// verandering om opnieuw te publishen
 
 /* jQuery Color Fade v0.1 * Released under the CC BY 3.0 License (http://creativecommons.org/licenses/by/3.0/) * https://github.com/matthewbj/jQuery-Color-Fade */
 (function(b){var c={init:function(a){var c=b.extend({fadeColor:"yellow",fadeTime:800,delayTime:600},a),d={position:"relative","z-index":1},e={height:"100%",width:"100%","background-color":c.fadeColor,position:"absolute",top:0,left:0,"z-index":"-1"};return this.each(function(a){b(this).css(d).append('<div id="colorFadeDiv'+a+'" class="colorFadeClass"></div>');b("#colorFadeDiv"+a).css(e).delay(c.delayTime).fadeOut(c.fadeTime)})},refade:function(a){a=b.extend({fadeColor:"yellow",fadeTime:800,delayTime:600},
